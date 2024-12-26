f3419e1003e7d4afa5e26c0a2c522d64bd14f4b5
SteveHYou <116049480+SteveHYou@users.noreply.github.com>
2024-12-26 15:14:27 -0500

    Doc

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'jsdom', // Allows vitest to have access to browser-level objects such as window
   },
 })
