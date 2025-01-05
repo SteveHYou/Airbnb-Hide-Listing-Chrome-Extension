d7dadcbee25c9e4c282448b75aa26c371703f012
SteveHYou <116049480+SteveHYou@users.noreply.github.com>
2025-01-05 12:02:31 -0500

    Update LISTING_ID_ATTRIBUTE_NAME

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const searchListingEleQuery = `[itemprop="itemListElement"]:not([data-testid="content-scroller"] *)` // Excludes "Available for similar dates" elements
 export const urlWithinSearchListingQuery = `meta[itemprop='url']`
-export const LISTING_ID_ATTRIBUTE_NAME = `listingId`
+export const LISTING_ID_ATTRIBUTE_NAME = `listing-id`
