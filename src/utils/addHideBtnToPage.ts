7bdff7394bdefc54d3f9861b55c81f239c1aa7a8
SteveHYou <116049480+SteveHYou@users.noreply.github.com>
2025-01-09 23:06:35 -0500

    Implement SetIntervalAsync.dynamic.setIntervalAsync usage in addHideBtnToPage. Add interval checker to hide listings we've hidden in the past

--- src/utils/addHideBtnToPage.ts
+++ src/utils/addHideBtnToPage.ts
@@ -1,14 +1,17 @@
 import { HIDE_BUTTON_TEXTCONTENT, LISTING_ID_ATTRIBUTE_NAME, searchListingEleQuery } from "../constants.js"; 
 import { createElement } from "./createElement.js";
+import { getAll } from "./database/chromeStorageLocalDb.js";
 import { getHideButtonId } from "./getHideButtonId.js";
 import { getSearchListings } from "./getSearchListings.js";
 import { onclickHideButton } from "./onclickHideButton.js";
 
 export function addHideBtnToPage() {
     // TODO: Not sure if to stop the setInterval after some time?
-    const interval = setInterval(() => {
+
+    //@ts-ignore
+    const interval = SetIntervalAsync.dynamic.setIntervalAsync(() => { 
         const listings = getSearchListings()
-        listings?.forEach(l => {
+        listings?.forEach(l => { 
             if (!l.hideBtn) {
                 console.log(`Listing ID ${l.listingId} is missing a hide button. Adding.`)
                 const btnEle = createElement(
@@ -22,6 +25,17 @@ export function addHideBtnToPage() {
                     }
                 )
                 l.element.appendChild(btnEle)
+            }
+        })
+    }, 250)
+
+    //@ts-ignore
+    const removeListingInterval = SetIntervalAsync.dynamic.setIntervalAsync(async () => {
+        const hiddenListings = await getAll('HiddenListing')
+        const listings = getSearchListings()
+        listings?.forEach(l => {
+            if (hiddenListings.find(hidden => hidden.id === l.listingId)) {
+                l.element.hidden = true
             }
         })
     }, 250)
